f4f43f41b326643176b445c0f92160a0ecd915d3
ayush8808 <1si20et008@sit.ac.in>
2023-04-27 14:56:30 +0530

    Video Show

--- venv/main.py
+++ venv/main.py
@@ -4,10 +4,13 @@ import cvzone
 import numpy as np
 
 #Video feed
-cap = cv2.VideoCapture()
+cap = cv2.VideoCapture('/Users/ayushmansharma/PycharmProjects/parkingCounter/venv/carPark.mp4')
 
 while True:
+    #to make the video run in loop 
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT): #if current frame== Total frame
+        cap.set(cv2.CAP_PROP_FRAME_COUNT,0) #if so then current frame =0 (i.e it'll again start from frame 0)
 
-    success,img = cap.read() #to get the frame in video
+    success, img = cap.read() #to get the frame in video
     cv2.imshow("Image",img)  #to show the video
     cv2.waitKey(1)          #video speed
