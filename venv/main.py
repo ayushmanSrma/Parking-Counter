62c8af7abf205b6e9e2757c9d4693deb68388984
ayush8808 <1si20et008@sit.ac.in>
2023-04-27 15:22:39 +0530

    Video File

--- venv/main.py
+++ venv/main.py
@@ -9,8 +9,12 @@ cap = cv2.VideoCapture('/Users/ayushmansharma/PycharmProjects/parkingCounter/ven
 while True:
     #to make the video run in loop 
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT): #if current frame== Total frame
-        cap.set(cv2.CAP_PROP_FRAME_COUNT,0) #if so then current frame =0 (i.e it'll again start from frame 0)
+        cap.set(cv2.CAP_PROP_POS_FRAMES,0) #if so then current frame =0 (i.e it'll again start from frame 0)
+
+   
 
     success, img = cap.read() #to get the frame in video
-    cv2.imshow("Image",img)  #to show the video
+    cv2.imshow('Image',img)  #to show the video
     cv2.waitKey(1)          #video speed
+
+    
